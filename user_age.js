b11b9352aa62db8304d46a00f803c91d0df610f8
Daria Trip <daria.trip001@gmail.com>
2021-04-24 04:21:54 +0300

    changed age outputs

--- user_age.js
+++ user_age.js
@@ -2,34 +2,54 @@
 
 function DisplayAge() {
 
-    let d = new Date();
-    let input = document.getElementById("user_date_of_birth").value;
-    if (input == "") return;
-    let user = new Date(input);
-    //calculate the Time zone Offset from user's time 
-    let offset = new Date().getTimezoneOffset() * 1000 * 60;
-    let newtimeinmilisec = d.getTime() - user.getTime() - offset;
-    let sec = parseInt(newtimeinmilisec / 1000);
-    let min = parseInt(sec / 60);
-    let ore = parseInt(min / 60);
-    let zi = parseInt(ore / 24);
+  let d = new Date();
+  let input = document.getElementById("user_date_of_birth").value;
+  if (input == "") return;
+  let user = new Date(input);
+  //calculate the Time zone Offset from user's time 
+  let offset = new Date().getTimezoneOffset() * 1000 * 60;
+  let newtimeinmilisec = d.getTime() - user.getTime() - offset;
+  let sec = parseInt(newtimeinmilisec / 1000);
+  let min = parseInt(sec / 60);
+  let ore = parseInt(min / 60);
+  let zi = parseInt(ore / 24);
+
+  let user_year = user.getFullYear();
+    let current_year = d.getFullYear();
+
+    let user_years=current_year-user_year;
+
+    let user_month = user.getMonth()+1;
+    let current_month = d.getMonth();
+
+    let user_day = user.getDate();
+    let current_day = d.getDate();
+
+    if(((user_month>current_month)||((user_month>current_month)&&(user_day>current_day)))&&user_years>0)
+    {
+      user_years--;
+    }
+
+  // ShowTime(zi, ore ,min, sec)
+  ShowTime(user_years);
+
+}
+
+function ShowTime(user_years) {
   
-    ShowTime(zi, ore ,min, sec);
-  
-  }
-  
-  function ShowTime(zi, ore, min, sec) {
+  let text1 = "You have lost ";
+  let age = user_years+" years";
+  let text2 =" playing video games. " + "<br><br><br>"
     
-    let text = "You have already lost this time "+"<br>"+"in your life playing video games: " + "<br><br>"
-      + "days: " + zi + "<br>"
-      + "hours: " + ore + "<br>"
-      + "minutes: " + min + "<br>"
-      + "seconds: " + sec + "<br><br>"
-      + "Now it`s time to make it real!"
-      ;
-    document.getElementById("output2").innerHTML = text;
-  }
-  
-  setInterval(DisplayAge, 1000);
+    + "Now it`s time to make it real 👌"
+    ;
+  document.getElementById("output2").innerHTML = text1;
+  document.getElementById("output_age").innerHTML= age;
+  document.getElementById("output3").innerHTML = text2;
+}
+
+setInterval(DisplayAge, 1000);
+
+
   
   
